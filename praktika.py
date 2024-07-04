f4f40f4fe4bb41b86321dd69a850b3762c8e716d
Oier Amelibia <149957383+oierzurbeltz@users.noreply.github.com>
2024-07-04 13:23:31 +0200

    Add files via upload

---
+++ praktika.py
@@ -0,0 +1,9 @@
+import weka.core.jvm as jwm
+from weka.core.converters import Loader
+from weka.classifiers import Classifier, Evaluation
+from weka.filters import Filter
+from weka.core.classes import Random
+
+jwm.start()
+## Hemen kodigoa
+jwm.stop()
